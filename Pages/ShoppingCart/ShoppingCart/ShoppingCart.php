9e1c86b01a80421ff598ac0b5a1c33354f427139
aelnajard <aelnajard@gmail.com>
2022-05-16 07:49:14 -0400

    ...

---
+++ Pages/ShoppingCart/ShoppingCart/ShoppingCart.php
@@ -0,0 +1,65 @@
+<style media="screen">
+  .shoppingCart{
+    position: relative;
+    padding: 40px 0px;
+  }
+  .containerShoppingCart{
+    position: relative;
+    width: 90%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    min-height: 70vh;
+  }
+  .productosContainer{
+    position: relative;
+    padding: 2px;
+    width: 60.3%;
+    background-color: orange;
+  }
+  .productosContainer h3{
+    font-size: 1.4em;
+    font-weight: 300;
+    margin: 0 0 40px 0;
+  }
+  .priceContainer{
+    position: relative;
+    padding: 2px;
+    width: 30%;
+    background-color: brown;
+  }
+  .boxShoppingCart{
+    position: relative;
+    padding: 2px;
+    background-color: red;
+    width: 95%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+</style>
+<section class="shoppingCart">
+  <div class="containerShoppingCart">
+    <div class="productosContainer">
+      <h3>Hello Cat, this are the products that you have add at your shopping cart</h3>
+      <div class="boxShoppingCart">
+        <div class="boxImageShoppingCart"> <!-- SC -> Shpping Cart-->
+
+        </div>
+        <div class="contectShoppingCart">
+
+        </div>
+        <div class="priceShoppingCart">
+
+        </div>
+      </div>
+    </div>
+    <div class="priceContainer">
+
+    </div>
+  </div>
+</section>
+<script type="text/javascript">
+
+</script>
